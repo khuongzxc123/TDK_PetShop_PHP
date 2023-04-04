ad7f8f71871e7888f05914ce61f0854f590fdba5
khuongzxc123 <khuongzxc123@gmail.com>
2023-04-04 07:31:00 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table tdk_petshop.category: ~2 rows (approximately)
+-- Dumping data for table tdk_petshop.category: ~3 rows (approximately)
 DELETE FROM `category`;
 INSERT INTO `category` (`Id`, `CateName`) VALUES
 	(1, 'Chó'),
@@ -44,9 +44,9 @@ CREATE TABLE IF NOT EXISTS `chitiethoadon` (
   KEY `HoaDonId` (`HoaDonId`),
   CONSTRAINT `chitiethoadon_ibfk_2` FOREIGN KEY (`ProductId`) REFERENCES `product` (`Id`),
   CONSTRAINT `chitiethoadon_ibfk_3` FOREIGN KEY (`HoaDonId`) REFERENCES `hoadon` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table tdk_petshop.chitiethoadon: ~18 rows (approximately)
+-- Dumping data for table tdk_petshop.chitiethoadon: ~19 rows (approximately)
 DELETE FROM `chitiethoadon`;
 INSERT INTO `chitiethoadon` (`Id`, `HoaDonId`, `ProductId`, `Quantity`) VALUES
 	(13, '640d682e00835', 1, 3),
@@ -67,7 +67,8 @@ INSERT INTO `chitiethoadon` (`Id`, `HoaDonId`, `ProductId`, `Quantity`) VALUES
 	(28, '6415d128484fc', 7, 1),
 	(29, '6415d128484fc', 8, 1),
 	(30, '6415d128484fc', 9, 1),
-	(31, '6415d1da9dfd3', 1, 1);
+	(31, '6415d1da9dfd3', 1, 1),
+	(32, '642252a789075', 1, 1);
 
 -- Dumping structure for table tdk_petshop.hoadon
 CREATE TABLE IF NOT EXISTS `hoadon` (
@@ -88,7 +89,8 @@ INSERT INTO `hoadon` (`Id`, `Day`, `UserId`, `Total`, `Address`) VALUES
 	('640d7f0d0fe5b', '2023-03-12 07:28:13', 2, 1000000, '107/11 Bùi Viện'),
 	('641035dbd732f', '2023-03-14 08:52:43', 2, 450000, '100 Trần Hưng Đạo'),
 	('6415d128484fc', '2023-03-18 14:56:40', 1, 1595000, '100/25/35b trần hưng đạo'),
-	('6415d1da9dfd3', '2023-03-18 21:59:38', 1, 100000, '100/25/35b trần hưng đạo');
+	('6415d1da9dfd3', '2023-03-18 21:59:38', 1, 100000, '100/25/35b trần hưng đạo'),
+	('642252a789075', '2023-03-28 09:36:23', 1, 100000, '100/25/35b trần hưng đạo');
 
 -- Dumping structure for table tdk_petshop.product
 CREATE TABLE IF NOT EXISTS `product` (
@@ -104,10 +106,10 @@ CREATE TABLE IF NOT EXISTS `product` (
   CONSTRAINT `product_ibfk_1` FOREIGN KEY (`CategoryId`) REFERENCES `category` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table tdk_petshop.product: ~8 rows (approximately)
+-- Dumping data for table tdk_petshop.product: ~9 rows (approximately)
 DELETE FROM `product`;
 INSERT INTO `product` (`Id`, `Name`, `Price`, `Image`, `Quantity`, `Unit`, `CategoryId`) VALUES
-	(1, 'Benefits', 100000, '6404d04f85384.jpg', 90, 'Hộp', 1),
+	(1, 'Benefits', 100000, '6404d04f85384.jpg', 89, 'Hộp', 1),
 	(2, 'Biolin', 200000, '6404d9458f292.png', 96, 'Hộp', 1),
 	(3, 'Smart Heart', 150000, '6404da4a0c058.jpg', 95, 'Hộp', 1),
 	(4, 'Premium', 120000, '6404da5baa3b3.png', 99, 'Hộp', 1),
@@ -166,8 +168,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table tdk_petshop.user: ~3 rows (approximately)
 DELETE FROM `user`;
 INSERT INTO `user` (`Id`, `UserName`, `Pass`, `FullName`, `Avata`, `RoleId`, `Token`, `Email`, `Status`) VALUES
-	(1, 'khuongzxc123', '$2y$10$uAo06Dorh2Ly05zjMyTGb.OjTix8OOJDAETybUKSlhmIHqRGZOKbG', 'Trần Văn Khương', '640feb4cac3f6.png', 1, NULL, NULL, 0),
-	(2, 'hugoloc1005', '$2y$10$uNFB71jyBccU70tjFfQeOeika3Jh9Az8oDx4KW0.VB5aO1petuWmK', 'Thú Ăn Tái', '6406f990ab24c.png', 2, '393266', 'hugoloc1005@gmail.com', 1),
+	(1, 'khuongzxc123', '$2y$10$uAo06Dorh2Ly05zjMyTGb.OjTix8OOJDAETybUKSlhmIHqRGZOKbG', 'Trần Văn Khương', '64225bf3d898e.jpg', 1, NULL, NULL, 0),
+	(2, 'hugoloc1005', '$2y$10$uWqXxqQLKKytU5iNp.7aDum5APFShR26FoNbZ5MkBjuhHW5/yRBrC', 'Thú Ăn Tái', '6406f990ab24c.png', 2, '904046', 'hugoloc1005@gmail.com', 1),
 	(3, 'hugoloc1003', '$2y$10$gFPi.yW4K88u6yZ245JD4u53DVyGqa.i5LjyF0JhIOg9GPYHY3UCW', 'Trần Văn Khương', '6406fcb2b13f6.jpg', 2, '536489', 'hugoloc1003@gmail.com', 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
